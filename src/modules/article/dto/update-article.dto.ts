4597e0434f0c962925d06fce38e5bb480f65c89e
thangdp-ekoios <thang.duong@ekoios.vn>
2022-06-24 17:54:23 +0700

    fix id

--- src/modules/article/dto/update-article.dto.ts
+++ src/modules/article/dto/update-article.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 import { CreateArticleDto } from './create-article.dto';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto {
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  image: string;
+
+  @IsString()
+  @IsOptional()
+  content: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsNumber()
+  @IsOptional()
+  isActive: number;
+}
